1b3aca0e7e8123b14e82311353a263534fb8b847
Yuuki Tanaka <y.tanaka@everyleaf.com>
2012-05-16 15:09:13 +0900

    add toppage

--- config/routes.rb
+++ config/routes.rb
@@ -48,9 +48,9 @@ Fbtest8::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
-  # See how all your routes lay out with "rake routes"
+  match "/auth/:provider/callback" => "sessions#create"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
