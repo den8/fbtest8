807371a1cdbd9bd6bdfd3c6328d2432aff4a76e4
Yuuki Tanaka <y.tanaka@everyleaf.com>
2012-05-16 14:05:04 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbtest8::Application.config.secret_token = 'a8a8b62124c41c92f75190e6c61e6104f9b3616d9c1d8ca9cfa4829d3a665508f314332f86f604918be41135b35aaa08caddc022bad4b7be23c6448d0e3421e2'
